7d8734b35be8aeb2fe5ddc8db672851a94fd27c1
Rpsl <gmail@rpsl.info>
2016-11-28 00:49:21 +0300

    add compile.sh add callkeeper.ru

---
+++ compile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+HEAD="[Adblock Plus 1.1]
+! Title: Lead Generator
+! Expires: 4 days (update frequency)
+! Homepage: https://github.com/Rpsl/adblock-leadgenerator-list
+!-----------------General tracking systems-----------------!
+! *** Fetched from: https://raw.githubusercontent.com/Rpsl/adblock-leadgenerator-list/master/list/list.txt ***"
+
+RULES=$(<rules.txt)
+
+echo "$HEAD" > ./misc/tmp.txt
+echo "$RULES"|sort >> ./misc/tmp.txt
+
+./misc/addChecksum.py < ./misc/tmp.txt > ./list/list.txt
+rm ./misc/tmp.txt
